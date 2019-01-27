5f5f02db74e0e482377e4271970dfc46fc8bbc5d
Christophe Delobel <cdelobel.ext@simplon.co>
2019-01-27 17:20:41 +0100

    add Apple

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ window.onload = function(){
     var ctx;
     var delay = 10;
     var snakee;
+    var applee;
 
     init();
 
@@ -23,6 +24,7 @@ window.onload = function(){
         ctx = canvas.getContext("2d");
 
         snakee = new Snake([[6,4], [5,4], [4,4]], "right");
+        applee = new Apple([10,10]);
 
         refreshCanvas();
     }
@@ -32,7 +34,9 @@ window.onload = function(){
         ctx.clearRect(0,0,canvasWidth, canvasHeight);  
 
         snakee.advance();
-        snakee.draw()
+        snakee.draw();
+
+        applee.draw();
 
         setTimeout(refreshCanvas, delay);
     }
@@ -103,6 +107,24 @@ window.onload = function(){
             }
         };
     };
+
+    function Apple(position)
+    {
+        this.position = position;
+        this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle = "#33cc33";
+            ctx.beginPath();
+            var radius = blockSize / 2;
+            var x = position[0] * blockSize + radius;
+            var y = position[1] * blockSize + radius;
+            ctx.arc(x,y, radius, 0, Math.PI*2, true);
+            ctx.fill();
+            ctx.restore();
+        }
+    }
+
 
     document.onkeydown = function handleKeyDown(e)
     {
